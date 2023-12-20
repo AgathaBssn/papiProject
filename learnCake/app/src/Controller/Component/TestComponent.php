89f2ec1ec223b1edb26d7d226319308b30bbd5aa
AgathaBssn <agatha.basson@gmail.com>
2023-12-20 13:15:27 +0100

    Init project

--- learnCake/app/src/Controller/Component/TestComponent.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Controller\Component;
-
-use Cake\Controller\Component;
-
-class TestComponent extends Component
-{
-
-}
--- learnCake/app/templates/Articles/view.php
+++
@@ -1,4 +0,0 @@
-<h1><?= h($element->title) ?></h1>
-<p><?= h($element->body) ?></p>
-<p><small>Created : <?= h($element->created->format(DATE_RFC1036)) ?></small></p>
-<p><?= $this->Html->link('Edit',['action' => 'edit', $element->slug])?></p>
